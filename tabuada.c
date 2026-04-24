d75424b50797b62c371c26f453ce6d2dce286098
Allan <allangomes_07@outlook.com>
2026-04-24 18:31:32 -0300

    inicio

---
+++ tabuada.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float n1;
+    float n2;
+    char operacao;
+    float resultado;
+
+    printf("Digite um número: \n");
+    scanf(" %f", &n1);
+
+    return 0;
+
+}
