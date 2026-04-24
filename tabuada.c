15fb4a6883fb702ab1dd772381bbabd281ccf774
Allan <allangomes_07@outlook.com>
2026-04-24 18:46:14 -0300

    término da calculadora

--- tabuada.c
+++ tabuada.c
@@ -6,8 +6,33 @@ int main(){
     float resultado;
 
     printf("Digite um número: \n");
-    scanf(" %f", &n1);
+    scanf("%f", &n1);
 
+    printf("Digite outro número: \n");
+    scanf("%f", &n2);
+
+    printf("Qual operador você deseja usar? (+), (-), (x), (/) \n");
+    scanf(" %c", &operacao);
+
+    if (operacao == '+'){
+        resultado = n1 + n2 ;
+        printf("A soma entre %.2f e %.2f é: %.2f", n1, n2, resultado);
+    }
+    else if (operacao == '-'){
+        resultado = n1 - n2 ;
+        printf("A subtração entre %.2f e %.2f é: %.2f", n1, n2, resultado);
+    }
+    else if (operacao =='x' || operacao =='X'){
+        resultado = n1 * n2;
+        printf("A multiplicação entre %.2f e %.2f é: %.2f", n1, n2 , resultado);
+    }
+    else if(operacao=='/'){
+        resultado = n1 / n2;
+        printf("A divisão entre %.2f e %.2f é: %.2f", n1, n2, resultado);
+    }
+    else{
+        printf("Essa operação não existe/não está disponível!");
+    }
     return 0;
 
 }
